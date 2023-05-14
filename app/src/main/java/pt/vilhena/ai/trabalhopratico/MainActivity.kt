87cfcb7a27fcfe4a240f6e915d76e6e7d3277b13
DIOGO <diogodevilhena@sapo.pt>
2023-05-14 17:15:52 +0100

    * Upgraded to AGP 8 * Initial Permissions

--- app/src/main/java/pt/vilhena/ai/trabalhopratico/MainActivity.kt
+++ app/src/main/java/pt/vilhena/ai/trabalhopratico/MainActivity.kt
@@ -1,16 +1,39 @@
 package pt.vilhena.ai.trabalhopratico
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import pt.vilhena.ai.trabalhopratico.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var isGranted = false
+
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { permission ->
+            isGranted = permission ?: isGranted
+        }
+
+        requestPermissions()
+    }
+
+    private fun requestPermissions() {
+        isGranted = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+
+        if (!isGranted) {
+            requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
     }
 }
