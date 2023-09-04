342b4c0d43d01a1f115403ffebeea9b0c5542eb2
DIOGO <diogodevilhena@sapo.pt>
2023-09-04 19:34:46 +0100

    * Removed Running activity and added Standing Activity

--- app/src/main/java/pt/vilhena/ai/trabalhopratico/data/common/ActivitiesEnum.kt
+++ app/src/main/java/pt/vilhena/ai/trabalhopratico/data/common/ActivitiesEnum.kt
@@ -4,7 +4,7 @@ import pt.vilhena.ai.trabalhopratico.R
 
 enum class ActivitiesEnum(val activity: String, val stringResource: Int, val animationResource: Int) {
     WALKING("walking", R.string.walking, R.raw.walking_animation),
-    RUNNING("running", R.string.running, R.raw.running_animation),
+    STANDING("standing", R.string.standing, R.raw.standing_animation),
     CLIMBING_UP_STAIRS("upstairs", R.string.climbing_up_stairs, R.raw.climbing_up_stairs_animation),
     CLIMBING_DOWN_STAIRS("downstairs", R.string.climbing_down_stairs, R.raw.climbing_down_stairs_animation),
 }
